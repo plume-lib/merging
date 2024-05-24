3e33bca37eaa0e429751b81250e31bdaa5f410c1
Michael Ernst <mernst@cs.washington.edu>
2024-05-24 14:16:23 -0700

    Improve comments

--- src/main/sh/git-mergetool-on-all.sh
+++ src/main/sh/git-mergetool-on-all.sh
@@ -11,6 +11,7 @@
 toplevel=$(git rev-parse --show-toplevel)
 merge_head_file="$toplevel/.git/MERGE_HEAD"
 if [ -f "$merge_head_file" ] ; then
+    # A merge is in progress.
     if [ "$(wc -l <"$merge_head_file")" -ge 2 ] ; then
         echo "git-mergetool-on-all.sh: Can't handle octopus merge."
         exit 1
@@ -21,6 +22,7 @@ elif git rev-parse HEAD^3 >/dev/null 2>/dev/null ; then
     echo "git-mergetool-on-all.sh: Can't handle octopus merge."
     exit 1
 elif git rev-parse HEAD^2 >/dev/null 2>/dev/null ; then
+    # A merge has just occurred.
     # I'm concerned that `git mergetool` isn't applicable here and I might need to do more work.
     LEFT_REV="$(git rev-parse HEAD^1)"
     RIGHT_REV="$(git rev-parse HEAD^2)"
@@ -31,15 +33,15 @@ fi
 
 BASE_REV="$(git merge-base "$LEFT" "$RIGHT")"
 
-echo "BASE_REV ${BASE_REV} LEFT_REV ${LEFT_REV} RIGHT_REV ${RIGHT_REV}"
+# echo "BASE_REV ${BASE_REV} LEFT_REV ${LEFT_REV} RIGHT_REV ${RIGHT_REV}"
 
 # Discover all files that are different in all 3 versions.
 readarray -t differing_files < <(comm -12 <(comm -12 <(git diff --name-only "${BASE_REV}..${LEFT_REV}" | sort) <(git diff --name-only "${BASE_REV}..${RIGHT_REV}" | sort)) <(git diff --name-only "${LEFT_REV}..${RIGHT_REV}" | sort))
 
-# If we are *after* a merge (that is, HEAD is a commit with two parents), then `git mergetool` will never take any action.
-# We must check out all the necessary files.
-# Do that regardless!
-
+# Unfortunately, mergetool_command lacks the quoting that appears in the git configuration file.
+# This might lead to trouble with filenames that contain spaces.
+# I'm not sure why `git config` reports it without the quotes.
+mergetool_command="$(git config --get mergetool."$(git config --get merge.tool)".cmd)"
 
 for file in "${differing_files[@]}" ; do
   basefile="$(mktemp -p /tmp "base-XXXXXX-$(basename "$file")")"
@@ -49,7 +51,7 @@ for file in "${differing_files[@]}" ; do
   rightfile="$(mktemp -p /tmp "right-XXXXXX-$(basename "$file")")"
   git show "$RIGHT_REV:$file" > "$rightfile"
 
-  command="export BASE='$basefile'; export LOCAL='$leftfile'; export REMOTE='$rightfile'; export MERGED='$file'; $(git config --get mergetool.merge-java.cmd)"
-  echo "$command"
+  command="export BASE='$basefile'; export LOCAL='$leftfile'; export REMOTE='$rightfile'; export MERGED='$file'; $mergetool_command"
+  eval "$command"
 
 done
